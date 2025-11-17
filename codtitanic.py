9f6dc0d66253f2d8cae0ecc9f76c1f5d450b018a
SofiaaaMeneses <menesescorvalansofia@gmail.com>
2025-11-17 09:51:01 -0300

    Update codtitanic.py

--- codtitanic.py
+++ codtitanic.py
@@ -46,6 +46,15 @@ st.write("## Sobrevivientes por sexo")
 
 survivors_by_sex = df.groupby("Sex")["Survived"].sum()
 
+#Slider Bar
+
+with st.sidebar:
+  st.write("# Opciones")
+
+  div = st.slider("Número de bins:", 0, 10, 2)
+
+  st.write("Bins", div)
+  
 #Gráfico
 
 fig2, ax2 = plt.subplots(figsize=(8,3))
