dc354d17f4b14f46eb586e20c5ce38cf4b9e4083
SofiaaaMeneses <menesescorvalansofia@gmail.com>
2025-11-17 16:34:18 -0300

    Update codtitanic.py

--- codtitanic.py
+++ codtitanic.py
@@ -85,7 +85,7 @@ with st.sidebar:
 df_filtrado = df.copy()
 df_filtrado = df_filtrado[
     df_filtrado["Age"].between(rango_edad[0], rango_edad[1]) &
-    (df_filtrado["Fare"] <= max_fare)
+    (df_filtrado["Fare"] <= max_fare)]
 
 
 with st.container():
